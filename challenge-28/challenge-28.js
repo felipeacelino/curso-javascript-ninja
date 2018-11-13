a527d6770702a69238727e84f16136765c3f5188
Felipe Acelino <felipeacelino@hotmail.com>
2018-11-13 00:55:17 -0200

    challenge-28

--- challenge-28/challenge-28.js
+++ challenge-28/challenge-28.js
@@ -1,3 +1,6 @@
+(function() {
+  'use strict';
+  
   /*
   No HTML:
   - Crie um formulário com um input de texto que receberá um CEP e um botão
@@ -25,3 +28,94 @@
   - Utilize a lib DOM criada anteriormente para facilitar a manipulação e
   adicionar as informações em tela.
   */
+
+  var ajax = new XMLHttpRequest();
+
+  var $cep = new DOM('#cep');
+  var $buscar = new DOM('#buscar');
+  var $status = new DOM('#status');
+  var $logradouro = new DOM('#logradouro');
+  var $bairro = new DOM('#bairro');
+  var $cidade = new DOM('#cidade');
+  var $estado = new DOM('#estado');
+
+  function getCep() {
+    return clearCep($cep.val());
+  }
+
+  function clearCep(string) {
+    return string.replace(/\D/g, '');
+  }
+
+  function isValidCep(cep) {
+    return !!cep && cep.length === 8;
+  }
+
+  function setMessageStatus(message) {
+    $status.html(message);
+  }
+
+  function showAddress(address) {
+    $logradouro.val(address.address);
+    $bairro.val(address.district);
+    $cidade.val(address.city);
+    $estado.val(address.state);
+  }
+
+  function resetForm() {
+    setMessageStatus('Aguardando solicitação.');
+    $logradouro.val('');
+    $bairro.val('');
+    $cidade.val('');
+    $estado.val('');
+  }
+
+  function buscarCep(cep) {  
+
+    setMessageStatus('Buscando informações para o CEP ' + cep + '...');
+
+    ajax.open('GET', 'http://apps.widenet.com.br/busca-cep/api/cep.json?code=' + cep);
+    ajax.send();
+    ajax.addEventListener('readystatechange', function() {
+      if (isValidResponse()) {
+        try {
+          var response = JSON.parse(ajax.responseText);
+          if (response.status === 1) {
+            setMessageStatus('Endereço referente ao CEP ' + cep + ':');
+            showAddress(response);
+          } else if (response.status === 0) {
+            setMessageStatus('Não encontramos o endereço para o CEP ' + cep + '.');
+          } else {
+            setMessageStatus(response.message);
+          }
+        } catch(e) {
+          setMessageStatus('Não foi possível obter o endereço para o CEP informado.');
+        }
+      }
+    }, false);
+  }
+
+  function isValidResponse() {
+    return ajax.readyState === 4 && ajax.status === 200;
+  }
+
+  $buscar.on('click', () => {
+
+    resetForm();
+
+    var cep = getCep();
+
+    if (!isValidCep(cep)) {
+      setMessageStatus('Informe um CEP válido!');
+      return false;
+    }
+
+    buscarCep(cep);
+      
+  });
+  
+  $cep.on('keydown keyup', () => {
+    $cep.val($cep.val().replace(/\D/g, ''));
+  });
+  
+})();
---
+++ challenge-28/dom.js
@@ -0,0 +1,97 @@
+function DOM(elements) {   
+    this.element = this.getDOMElements(elements);
+}
+
+DOM.prototype.getDOMElements = function getDOMElements(elements) {
+    return document.querySelectorAll(elements);
+};
+
+DOM.prototype.on = function on(events, callback) {
+    var events = events.split(' ');
+    Array.prototype.forEach.call(this.element, function(element) {
+        events.forEach((event) => {
+            element.addEventListener(event, callback, false);
+        });
+    });
+};
+
+DOM.prototype.off = function off(event, callback) {
+    Array.prototype.forEach.call(this.element, function(element) {
+        element.removeEventListener(event, callback, false);
+    });
+};
+
+DOM.prototype.get = function get() {
+    return this.element;
+};
+
+DOM.prototype.forEach = function forEach() {
+    return Array.prototype.forEach.apply(this.element, arguments);
+}
+
+DOM.prototype.map = function map() {
+    return Array.prototype.map.apply(this.element, arguments);
+}
+
+DOM.prototype.filter = function filter() {
+    return Array.prototype.filter.apply(this.element, arguments);
+}
+
+DOM.prototype.reduce = function reduce() {
+    return Array.prototype.reduce.apply(this.element, arguments);
+}
+
+DOM.prototype.reduceRight = function reduceRight() {
+    return Array.prototype.reduceRight.apply(this.element, arguments);
+}
+
+DOM.prototype.every = function every() {
+    return Array.prototype.every.apply(this.element, arguments);
+}
+
+DOM.prototype.some = function some() {
+    return Array.prototype.some.apply(this.element, arguments);
+}
+
+DOM.prototype.isArray = function isArray(param) {
+    return Object.prototype.toString.call(param) === '[object Array]';
+}
+
+DOM.prototype.isObject = function isObject(param) {
+    return Object.prototype.toString.call(param) === '[object Object]';
+}
+
+DOM.prototype.isFunction = function isFunction(param) {
+    return Object.prototype.toString.call(param) === '[object Function]';
+}
+
+DOM.prototype.isNumber = function isNumber(param) {
+    return Object.prototype.toString.call(param) === '[object Number]';
+}
+
+DOM.prototype.isString = function isString(param) {
+    return Object.prototype.toString.call(param) === '[object String]';
+}
+
+DOM.prototype.isBoolean = function isBoolean(param) {
+    return Object.prototype.toString.call(param) === '[object Boolean]';
+}
+
+DOM.prototype.isNull = function isNull(param) {
+    return Object.prototype.toString.call(param) === '[object Undefined]' || Object.prototype.toString.call(param) === '[object Null]';
+}
+
+DOM.prototype.val = function val() {
+    if (arguments.length === 0) {
+        return this.get()[0].value;
+    }
+    this.forEach((element) => {
+        element.value = arguments[0]; 
+    });   
+}
+
+DOM.prototype.html = function html(html) {
+    this.forEach((element) => {
+        element.innerHTML = html;
+    });
+}
